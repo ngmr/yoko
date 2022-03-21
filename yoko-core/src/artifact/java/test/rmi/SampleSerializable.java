f16a0cf7c6aa98138f60bcc4142ac1588b93f313
Neil Richards <7203556+ngmr@users.noreply.github.com>
2022-03-21 17:51:23 +0000

    TST: reorder field declarations to marshalling order

--- yoko-core/src/artifact/java/test/rmi/SampleSerializable.java
+++ yoko-core/src/artifact/java/test/rmi/SampleSerializable.java
@@ -1,11 +1,11 @@
 /**
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
-*  contributor license agreements.  See the NOTICE file distributed with
-*  this work for additional information regarding copyright ownership.
-*  The ASF licenses this file to You under the Apache License, Version 2.0
-*  (the "License"); you may not use this file except in compliance with
-*  the License.  You may obtain a copy of the License at
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -19,22 +19,22 @@ package test.rmi;
 
 import java.io.Serializable;
 import java.rmi.Remote;
-import java.util.List; 
+import java.util.List;
 
 public class SampleSerializable implements Serializable {
-	private org.omg.CORBA.Object corbaObj;
-	private test.rmi.SampleCorba sampleCorba;
-	private Remote remote;
-	private SampleRemote sampleRemote;
-	private Object remoteObj;
-	private Object serializableObj1, serializableObj2;
-	private Serializable serializable;
 	private int i = 0;
-    private List aVector; 
-	
+	private List aVector;
+	private org.omg.CORBA.Object corbaObj;
+	private Remote remote;
+	private Object remoteObj;
+	private test.rmi.SampleCorba sampleCorba;
+	private SampleRemote sampleRemote;
+	private Serializable serializable;
+	private Object serializableObj1, serializableObj2;
+
 	public void setInt(int i) { this.i = i; }
 	public int getInt() { return i; }
-	
+
 	public void setSampleCorba(SampleCorba sampleCorba) {
 		this.sampleCorba = sampleCorba;
 	}
@@ -54,17 +54,17 @@ public class SampleSerializable implements Serializable {
 	public void setSerializableObject(Serializable ser) {
 		this.serializableObj1 = this.serializableObj2 = ser;
 	}
-	
+
 	public org.omg.CORBA.Object getCorbaObj() {
 		return corbaObj;
 	}
-	
+
 	public Remote getRemote() { return remote; }
-	
+
 	public Object getRemoteObject() {
 		return remoteObj;
 	}
-	
+
 	public Object getSerializableObject() {
 		if(serializableObj1 == serializableObj2) {
 			return serializableObj1;
@@ -73,23 +73,23 @@ public class SampleSerializable implements Serializable {
 			throw new Error("Expected serializable objects to be == identical");
 		}
 	}
-    
-    public void setList(List l) {
-        aVector = l; 
-    }
-    
-    public List getList() {
-        return aVector; 
-    }
-	
+
+	public void setList(List l) {
+		aVector = l;
+	}
+
+	public List getList() {
+		return aVector;
+	}
+
 	public void setSerializable(Serializable s) {
 		this.serializable = s;
 	}
-	
+
 	public Serializable getSerializable() {
 		return serializable;
 	}
-	
+
 	public void setSampleRemote(SampleRemote sampleRemote) {
 		this.sampleRemote = sampleRemote;
 	}
